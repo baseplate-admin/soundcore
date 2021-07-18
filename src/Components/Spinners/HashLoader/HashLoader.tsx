5c54afb1f2e06bddd90653946892c9c0e257caec
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2021-07-18 12:19:57 +0600

    Update : Add JSS for hashloader

--- src/Components/Spinners/HashLoader/HashLoader.tsx
+++ src/Components/Spinners/HashLoader/HashLoader.tsx
@@ -1,12 +1,77 @@
+import { createUseStyles } from 'react-jss';
 import './scss/HashLoader.scoped.scss';
 
 export const HashLoader = () => {
+    const classes = useStyles();
     return (
-        <div className='hash_loader'>
+        <div className="hash_loader">
             <span className="wrapper">
-                <span className="style1" />
-                <span className="style2" />
+                <span className={classes.style1} />
+                <span className={classes.style2} />
             </span>
         </div>
     );
 };
+
+const HashColor: string = 'rgba(54, 215, 183, 0.75)';
+
+const useStyles = createUseStyles({
+    '@keyframes animation-1': {
+        '0%': {
+            width: '10px',
+            boxShadow: `${HashColor} 20px -10px, ${HashColor} -20px 10px`,
+        },
+        '35%': {
+            width: '50px',
+            boxShadow: `${HashColor} 0px -10px, ${HashColor} 0px 10px`,
+        },
+        '70%': {
+            width: '10px',
+            boxShadow: `${HashColor} -20px -10px, ${HashColor} 20px 10px`,
+        },
+        '100%': {
+            boxShadow: `${HashColor} 20px -10px, ${HashColor} -20px 10px`,
+        },
+    },
+    '@keyframes animation-2': {
+        '0%': {
+            height: '10px',
+            boxShadow: `${HashColor} 10px 20px, ${HashColor} -10px -20px`,
+        },
+        '35%': {
+            height: '50px',
+            boxShadow: `${HashColor} 10px 0px, ${HashColor} -10px 0px`,
+        },
+        '70%': {
+            height: '10px',
+            boxShadow: `${HashColor} 10px -20px, ${HashColor} -10px 20px`,
+        },
+        '100%': {
+            boxShadow: `${HashColor} 10px 20px, ${HashColor} -10px -20px`,
+        },
+    },
+    style1: {
+        position: 'absolute',
+        content: "''",
+        top: '50%',
+        left: '50%',
+        display: 'block',
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        transform: 'translate(-50%, -50%)',
+        animation: '2s ease 0s infinite normal none running $animation-1',
+    },
+    style2: {
+        position: 'absolute',
+        content: "''",
+        top: '50%',
+        left: '50%',
+        display: 'block',
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        transform: 'translate(-50%, -50%)',
+        animation: '2s ease 0s infinite normal none running $animation-2',
+    },
+});
