ba6dfbc61256e869aec9e8a8eac95acdba76febf
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2021-07-19 13:23:04 +0600

    Update: Fix Navbar styling

--- src/Components/App/Navbar/Navbar.tsx
+++ src/Components/App/Navbar/Navbar.tsx
@@ -1,23 +1,26 @@
+import { IoMenuOutline, IoSearchOutline } from 'react-icons/io5';
+import { createUseStyles } from 'react-jss';
 import { Link } from 'react-router-dom';
-
 import brandLogo from '../../../Assets/Images/brand_logo.png';
-
-import { IoMenuSharp } from 'react-icons/io5';
-import { MdSearch } from 'react-icons/md';
 import { useAppDispatch, useAppSelector } from '../../../Hooks/Store/Hooks';
+import { RoutingPath } from '../../../Routes';
 import {
     leftMenuHidden,
     leftMenuShown,
     selectLeftMenuState,
 } from '../../../Store/Slices/NavbarSlice';
-import { RoutingPath } from '../../../Routes';
-import { createUseStyles } from 'react-jss';
+import { useMediaQuery } from 'react-responsive';
+import { Fragment } from 'react';
 
 export const Navbar = () => {
     const classes = useStyles();
+
+    const isMobile = useMediaQuery({
+        query: '(max-width: 767px)',
+    });
+
     const leftMenuState = useAppSelector(selectLeftMenuState);
     const dispatch = useAppDispatch();
-
     const handleHamburgerIconClick = () => {
         // Always returns true
         if (!leftMenuState.isHidden) {
@@ -27,130 +30,150 @@ export const Navbar = () => {
         }
     };
     return (
-        <div
-            className={`columns is-mobile top-navbar ${classes['top-navbar']}`}
-        >
-            <div id="hamburger_icon_wrapper_id" className="column is-narrow">
-                <IoMenuSharp
-                    color="white"
-                    style={{ transform: 'scale(1.6)' }}
-                    onClick={handleHamburgerIconClick}
-                />
-            </div>
-            <div className="column is-narrow">
-                <img
-                    alt=""
-                    className={`${classes.brand_image}`}
-                    src={brandLogo}
-                    width="120"
-                    height="40"
-                />
-            </div>
-            <div className="column">
-                <div className={`field ${classes.search_input_div}`}>
-                    <div
-                        className={`control has-icons-right ${classes.search_input_wrapper}`}
-                    >
-                        <input
-                            className={`input ${classes.search_input}`}
-                            type="text"
-                            placeholder="Search"
-                        />
-                        <span
-                            className={`icon is-small is-right ${classes.search_input_box_right_wrapper}`}
+        <>
+            <div className={`columns is-mobile is-centered ${classes.navbar}`}>
+                <div
+                    className="column is-2 is-one-fifth-mobile
+"
+                >
+                    <div className="columns is-mobile is-centered">
+                        <table
+                            className="table"
+                            style={{ backgroundColor: 'transparent' }}
                         >
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <div
+                                            className={
+                                                classes.items_translated_nav
+                                            }
+                                        >
+                                            <IoMenuOutline
+                                                color="white"
+                                                onClick={() => {
+                                                    handleHamburgerIconClick();
+                                                }}
+                                                style={{
+                                                    transform:
+                                                        'scale(2) translateX(8px) translateY(2px)',
+                                                }}
+                                            />
+                                        </div>
+                                    </td>
+                                    <td>
+                                        {/* If mobile Hide the image */}
+                                        {/* Else Show empty div  */}
+                                        {isMobile ? (
+                                            <Fragment></Fragment>
+                                        ) : (
+                                            <div
+                                                className={
+                                                    classes.image_wrapper
+                                                }
+                                            >
+                                                <div
+                                                    className={
+                                                        classes.items_translated_nav
+                                                    }
+                                                >
+                                                    <img
+                                                        src={brandLogo}
+                                                        alt=""
+                                                        width="112"
+                                                        height="28"
+                                                    ></img>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="column is-narrow"></div>
+                <div className="column is-1"></div>
+                <div className="column">
+                    <div className={classes.items_translated_nav}>
+                        <div className="field">
                             <div
-                                className={`box ${classes.search_input_box_right}`}
+                                className={`control has-icons-left has-icons-right ${classes.input_item_reverse_translate}`}
                             >
-                                <MdSearch
-                                    color="white"
-                                    style={{
-                                        transform: 'scale(1.4)',
-                                    }}
+                                <input
+                                    className={`input ${classes.input_item}`}
+                                    type="text"
+                                    placeholder="Search "
                                 />
+                                <span className="icon is-small is-left"></span>
+                                <span className="icon is-small is-right">
+                                    <IoSearchOutline
+                                        color="white"
+                                        style={{
+                                            transform: 'scale(1.5)',
+                                        }}
+                                    />
+                                </span>
                             </div>
-                        </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="column is-1"></div>
+
+                <div className="column is-narrow">
+                    <div className={` ${classes.items_translated_nav}`}>
+                        <Link
+                            className={`button is-rounded ${classes.button}`}
+                            to={RoutingPath.LOGIN_PAGE}
+                        >
+                            Login
+                        </Link>
                     </div>
                 </div>
             </div>
-            <div className="column is-narrow ">
-                {/* {% if not user.is_authenticated %} */}
-                <div className={`${classes.login_button_wrapper}`}>
-                    <Link
-                        className={`button is-rounded ${classes.login_button}`}
-                        to={RoutingPath.LOGIN_PAGE}
-                    >
-                        <p style={{ marginBottom: 5 }}>Login</p>
-                    </Link>
-                </div>
-                {/* {% elif user.is_authenticated %}
-                <figure className="image is-32x32 user_image">
-                <img alt=""
-                     src="https://bulma.io/images/placeholders/128x128.png"
-                     className="is-rounded"/>
-                </figure>
-                {% endif %} */}
-            </div>
-        </div>
+        </>
     );
 };
 
 const useStyles = createUseStyles({
-    'top-navbar': { backgroundColor: '#161616', color: 'white' },
-    brand_image: {
-        transform: 'translateY(0.6em) translateX(30px)',
-
-        '@media screen and (max-width: 767px)': {
-            transform: 'translateY(0.6em) translateX(20px) !important',
-        },
+    navbar: {
+        backgroundColor: '#161616',
+        height: 65,
     },
-    login_button: {
-        color: '#eaeaea !important',
-        backgroundColor: 'transparent !important',
-        borderColor: '#3b3838 !important',
-        borderWidth: '1px',
+
+    button: {
+        color: ' #e2dfda',
+        border: '1px solid #4c5759 !important',
         transition: '0.4s',
+        fontFamily: 'Nunito',
+        backgroundColor: '#191b1f !important',
+        transform: 'translateY(-5px) translateX(-5px)',
 
         '&:hover': {
-            color: '#a5a4a4 !important',
-            backgroundColor: '#101010 !important',
-            borderColor: '#3b3838 !important',
-            borderWidth: '1px',
+            color: '#e2dfda',
+            fontFamily: 'Nunito',
+            backgroundColor: '#121316 !important',
         },
     },
-    search_input_wrapper: {
-        flexGrow: 0.4,
-        transform: 'translateY(5px)',
+    input_item_reverse_translate: {
+        transform: 'translateY(-5px)', // <- Revert translation
+    },
 
-        '@media screen and (max-width: 767px)': {
-            display: 'none',
-        },
+    items_translated_nav: {
+        transform: 'translateY(10px)',
     },
-    search_input: {
-        backgroundColor: '#060606 !important',
-        border: 'transparent !important',
-        color: 'white !important',
-        borderRadius: '0',
+    image_wrapper: {
+        paddingLeft: '1em',
+    },
+    input_item: {
+        backgroundColor: `#131417 !important`,
+        border: `1px solid #4c5759 !important`,
+        color: `#e2dfda !important`,
 
         '&::placeholder': {
-            color: 'white !important',
-            opacity: 0.6,
+            color: '#e2dfda',
+            opacity: 0.5,
+            fontFamily: 'Nunito',
         },
-    },
-
-    search_input_box_right_wrapper: {
-        overflow: 'hidden',
-    },
-    login_button_wrapper: {
-        transform: 'translateY(5px) translateX(-5px)',
-    },
-    user_image: { transform: 'translateY(10px) translateX(-10px)' },
-    search_input_box_right: {
-        backgroundColor: '#323232 !important',
-        borderRadius: '0',
-    },
-    search_input_div: {
-        display: 'flex',
-        justifyContent: 'center',
     },
 });
